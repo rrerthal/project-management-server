a51850f093b1d1b29226a7a0e0c9ff61427905f9
Rodrigo Erthal <rrerthal@gmail.com>
2023-09-18 12:58:14 +0200

    implements full crud on projects

---
+++ routes/task.routes.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+
+const Task = require("../models/Task.model");
+const Project = require("../models/Project.model");
+
+//  POST /api/tasks  -  Creates a new task
+router.post("/tasks", (req, res, next) => {
+    const { title, description, projectId } = req.body;
+
+    const newTask = { 
+        title, 
+        description, 
+        project: projectId 
+    }
+
+    Task.create(newTask)
+        .then(newTask => {
+            return Project.findByIdAndUpdate(projectId, { $push: { tasks: newTask._id } });
+        })
+        .then(response => res.json(response))
+        .catch(err => {
+            console.log("Error creating new task...", err);
+            res.status(500).json({
+                message: "Error creating a new task",
+                error: err
+            });
+        });
+});
+
+
+module.exports = router;
