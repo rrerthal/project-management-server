aae8266fb12c53445b72ba1f3cc04afbb042120d
Rodrigo Erthal <rrerthal@gmail.com>
2023-09-19 21:37:40 +0200

    auth middleware

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,32 +1,21 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 
+const { isAuthenticated } = require("../middleware/jwt.middleware");
+
 const Task = require("../models/Task.model");
 const Project = require("../models/Project.model");
 
 //  POST /api/tasks  -  Creates a new task
-router.post("/tasks", (req, res, next) => {
-    const { title, description, projectId } = req.body;
+router.post("/tasks", isAuthenticated, (req, res, next) => {
+  const { title, description, projectId } = req.body;
 
-    const newTask = { 
-        title, 
-        description, 
-        project: projectId 
-    }
-
-    Task.create(newTask)
-        .then(newTask => {
-            return Project.findByIdAndUpdate(projectId, { $push: { tasks: newTask._id } });
-        })
-        .then(response => res.json(response))
-        .catch(err => {
-            console.log("Error creating new task...", err);
-            res.status(500).json({
-                message: "Error creating a new task",
-                error: err
-            });
-        });
+  Task.create({ title, description, project: projectId })
+    .then(newTask => {
+      return Project.findByIdAndUpdate(projectId, { $push: { tasks: newTask._id } } );
+    })
+    .then(response => res.json(response))
+    .catch(err => res.json(err));
 });
-
 
 module.exports = router;
